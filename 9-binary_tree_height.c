4f76511a62d1af83ed807c70f5d77d506b57b6c8
THOSSYN <mtafortunefinder@gmail.com>
2023-08-29 07:53:55 -0700

    Added: Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,18 +11,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
-	else
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
 	{
-		left_height = binary_tree_height(tree->left);
-		right_height = binary_tree_height(tree->right);
-
-		if (left_height > right_height)
-		{
-			return (1 + left_height);
-		}
-		else
-			return (1 + right_height);
+		return (1 + left_height);
 	}
+	else
+		return (1 + right_height);
 }
