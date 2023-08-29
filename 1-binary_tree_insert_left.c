e4279735aebb3f52fe2e3db051a9149f7f8ccdfd
THOSSYN <mtafortunefinder@gmail.com>
2023-08-29 06:23:10 -0700

    Added: 2-binary_tree_insert_right.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,9 +15,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	newnode = binary_tree_node(parent, value);
+	newnode = binary_tree_node(newnode, value);
 
-	newnode->n = value;
 	newnode->right = NULL;
 	newnode->parent = parent;
 	if (parent->left == NULL)
