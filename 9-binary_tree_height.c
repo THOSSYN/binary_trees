b2c9bdea2ad2915da1245686873e0daa23b4ab58
THOSSYN <mtafortunefinder@gmail.com>
2023-08-29 07:01:34 -0700

    Added: 4 more files

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: is the pointer to the root node
+ * Return: 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	
+	else
+	{
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+
+		if (left_height > right_height)
+		{
+			return (1 + left_height);
+		}
+		else
+			return (1 + right_height);
+	}
+}
