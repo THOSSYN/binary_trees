172e7e111d2844148858f21fb826bbe975e4a924
THOSSYN <mtafortunefinder@gmail.com>
2023-08-30 14:32:33 -0700

    Updated: 104-bin.....

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -9,10 +9,12 @@
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
+	binary_tree_t *new_head;
+
 	if (tree == NULL || tree->left == NULL)
 		return (tree);
 
-	binary_tree_t *new_head = tree->left;
+	new_head = tree->left;
 
 	tree->left = new_head->right;
 	if (new_head->right)
