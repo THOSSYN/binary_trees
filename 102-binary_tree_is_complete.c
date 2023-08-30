c2aa7de10590732849ebd9c0b196d30b8dc9e4fd
THOSSYN <mtafortunefinder@gmail.com>
2023-08-30 06:29:02 -0700

    Added: 102-binary...

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -11,12 +11,7 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	int l_path = binary_tree_is_complete(tree->left);
-	/*int r_path = binary_tree_is_complete(tree->right);*/
-
 	if (tree->left != NULL && tree->right != NULL)
 		return (1);
-	if (l_path && tree->right->left == NULL && tree->right->right != NULL)
-		return (0);
 	return (0);
 }
