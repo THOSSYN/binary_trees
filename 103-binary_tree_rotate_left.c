bee8b1fb9c263c55be63f3b778a8d4e222c88283
THOSSYN <mtafortunefinder@gmail.com>
2023-08-30 09:55:07 -0700

    added: 103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 		return (NULL);
 
 	binary_tree_t *new_tree = tree->right;
-	
+
 	tree->right = new_tree->left;
 
 	if (new_tree->left != NULL)
