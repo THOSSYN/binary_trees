7edf7300d396ba66007596dc52bb6da68b3f1462
THOSSYN <mtafortunefinder@gmail.com>
2023-08-31 07:42:46 -0700

    Added: Check for valid AVL

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is
+ * valid AVL tree
+ * @tree: pointer to root node
+ * Return: 1 if tree is valid AVL otherwise 0
+ */
+
+int balance_tree_height_avl(const binary_tree_t *tree);
+int max(int a, int b);
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (balance_tree_height_avl(tree) != -1);
+}
+
+int balance_tree_height_avl(const binary_tree_t *tree)
+{
+	int left_tree_hght, right_tree_hght;
+
+	if (tree == NULL)
+		return (0);
+
+	left_tree_hght = balance_tree_height_avl(tree->left);
+	if (left_tree_hght == -1)
+		return (-1);
+
+	right_tree_hght = balance_tree_height_avl(tree->right);
+	if (right_tree_hght == -1)
+		return (-1);
+
+	if (abs(left_tree_hght - right_tree_hght) > 1)
+		return (-1);
+
+	return (max(left_tree_hght, right_tree_hght) + 1);
+}
+
+int max(int a, int b)
+{
+	return ((a > b) ? a : b);
+}
