491c54195fd8b6aa9daf2f8d3157da98afa50cf3
THOSSYN <mtafortunefinder@gmail.com>
2023-09-01 08:13:25 -0700

    Corrected: 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,16 +7,32 @@
  * Return: 0 if tree is empty
  */
 
+int max(int a, int b);
+int height(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int l_path = 0, r_path = 0;
+	int left, right;
 
 	if (tree == NULL)
 		return (0);
+	left = height(tree->left);
+	right = height(tree->right);
 
-	if (tree->left)
-		l_path = (1 + binary_tree_balance(tree->left));
-	if (tree->right)
-		r_path = (1 + binary_tree_balance(tree->right));
-	return (l_path - r_path);
+	return (left - right);
+}
+int height(const binary_tree_t *tree)
+{
+         int l_path = 0, r_path = 0;
+
+         if (tree == NULL)
+                 return (0);
+
+	 l_path = height(tree->left);
+	 r_path = height(tree->right);
+	 return (max(l_path, r_path) + 1);
+}
+
+int max(int a, int b)
+{
+	return ((a > b) ? a : b);
 }
