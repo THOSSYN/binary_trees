70d5ba9a2d1bf411ccf7fa9c96671fa53ec00082
THOSSYN <mtafortunefinder@gmail.com>
2023-09-01 08:23:11 -0700

    Corrected: 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+int max(int a, int b);
+int height(const binary_tree_t *tree);
 /**
  * binary_tree_balance - measures the balance factor
  * of a binary tree
@@ -7,8 +9,6 @@
  * Return: 0 if tree is empty
  */
 
-int max(int a, int b);
-int height(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left, right;
@@ -20,18 +20,28 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (left - right);
 }
+/**
+ * height - finds the height of a binary tree
+ * @tree: pointer to root node
+ * Return: the maximum height
+ */
 int height(const binary_tree_t *tree)
 {
-         int l_path = 0, r_path = 0;
+	int l_path = 0, r_path = 0;
 
-         if (tree == NULL)
-                 return (0);
+	if (tree == NULL)
+		return (0);
 
-	 l_path = height(tree->left);
-	 r_path = height(tree->right);
-	 return (max(l_path, r_path) + 1);
+	l_path = height(tree->left);
+	r_path = height(tree->right);
+	return (max(l_path, r_path) + 1);
 }
-
+/**
+ * max - find the max value
+ * @a: first value
+ * @b: second value
+ * Return: the max value
+ */
 int max(int a, int b)
 {
 	return ((a > b) ? a : b);
